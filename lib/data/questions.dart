e6ddb5118b8d324680f8a22962c0c6cb4fdc54d8
Makanaki <ukahdaniel03@gmail.com>
2023-07-27 01:29:40 +0100

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,118 @@
+import 'package:godfrey_quiz/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What year was the University founded?',
+    [
+      '3 November 2009',
+      '1 November 2009',
+      '20 November 2009',
+      '15 November 2009',
+    ],
+  ),
+  QuizQuestion('Who was the first Vice Chancellor of the University?', [
+    'Rev.Fr.Prof.Dr. Christian Anieke',
+    'Archbishop Ignatius Ayau Kaigama',
+    'prof. Chidi Chukwuemeka Uhuegbu',
+    'Prof. Christian Nwachukwu Okeke',
+  ]),
+  QuizQuestion(
+    'How many Faculties are in the University?',
+    [
+      'Five',
+      'Six',
+      'Four',
+      'Seven',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the current Chairman of Board of Trustees, Godfrey Okoye University?',
+    [
+      'Prof. Dr. Joseph Ikechukwu Chidobem',
+      'Prof. Dr. charles Ikechukwu Chidobem',
+      'Prof. Dr. Joseph Chukwuemeka Chidobem',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'In what Academic Year was the faculty of law inaugurated?',
+    [
+      '2016/2017',
+      '2015/2016',
+      '2017/2018',
+      'None of the Above',
+    ],
+  ),
+  QuizQuestion(
+    ' Who was the first VC for-one-day, GOU?',
+    [
+      'Miss Scholastica Chinonso Ogbodo',
+      'Miss Scholastica Chinasa Ogbodo',
+      'Miss Scholastica Chinonso Ogwali',
+      'Miss Shantella Chinonso Ogbodo',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the Chancellor of GOU?',
+    [
+      'Archbishop Ignatius Ayau Kaigama',
+      'Rev.Fr.Prof.Dr. Christian Anieke',
+      'Rev.Fr.Dr. Christian Aneke',
+      'Rev.Fr.Prof.Dr. Christus Anieke',
+    ],
+  ),
+  QuizQuestion(
+    'What is the motto of Godfrey Okoye University?',
+    [
+      'Unity of Knowledge',
+      'Offering the Best Health Services',
+      'Knowledge and Godliness',
+      'Peace and Progress',
+    ],
+  ),
+  QuizQuestion(
+    'When was GOuni Radio Established?',
+    [
+      'October 2014',
+      'May 2014',
+      'November 2014',
+      'September 2014',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the Vice Chancellor of GOU?',
+    [
+      'Rev.Fr.Prof.Dr. Christian Anieke',
+      'Archbishop Ignatius Ayau Kaigama',
+      'Rev.Fr.Dr. Christian Aneke',
+      'Rev.Fr.Prof.Dr. Christus Anieke',
+    ],
+  ),
+  QuizQuestion(
+    'In what year did GOUni Radio win the best Campus Radio?',
+    [
+      '2016',
+      '2014',
+      '2015',
+      '2017',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the Pro Chancellor of the University?',
+    [
+      'Prof. Christian Nwachukwu Okeke',
+      'Prof. Christiana Nwachukwu Okeke',
+      'Prof. Christian Nworie Okeke',
+      'Prof. Christian Nwachukwu Okereke',
+    ],
+  ),
+  QuizQuestion(
+    'In what year did GOUni Radio win the best Campus Radio?',
+    [
+      '2016',
+      '2014',
+      '2015',
+      '2017',
+    ],
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:godfrey_quiz/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
